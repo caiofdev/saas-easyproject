1151da6f42dcf8d494c95bd1d2cf5c08e956abe7
Caio Reis <caiofreis2005@gmail.com>
2025-10-02 13:24:12 -0300

    feat: initialize auth package with models, permissions, and subjects

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,51 @@
+import type { MongoAbility, CreateAbility } from '@casl/ability';
+import type { User } from './models/user';
+import { userSubject } from './subjects/user';
+import { projectSubject } from './subjects/project';
+
+import { z } from 'zod';
+import { AbilityBuilder, createMongoAbility } from '@casl/ability';
+import { permissions } from './permissions';
+import { organizationSubject } from './subjects/organization';
+import { inviteSubject } from './subjects/invite';
+import { billingSubject } from './subjects/billing';
+
+export * from './models/organization'
+export * from './models/project'
+export * from './models/user'
+
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+
+  z.tuple([
+    z.literal('manage'),
+    z.literal('all')
+  ])
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
+
+export type AppAbility = MongoAbility<AppAbilities>;
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>;
+
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
+
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permission for ${user.role} not found.`)
+  }
+
+  permissions[user.role](user, builder)
+
+  const ability = builder.build({
+    detectSubjectType(subject) {
+        return subject.__typename
+    },
+  })
+
+  return ability
+}
